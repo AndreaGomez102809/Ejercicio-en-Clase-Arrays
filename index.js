2713a8468f8ab48a01a1d585df69fb47fa167c3a
unknown <magomez1028@gmail.com>
2020-07-01 16:37:25 -0500

    ejercicio arrays

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hola Mundo");
+
+//Ejercicio en Clase Arrays//
+
+var arrays = [
+    ['p','s','l','o','j'],
+    ['g','e','i','a','k'],
+    ['n','b','r','u','b'],
+    ['c','r','h','r','s'],
+    ['a','v','z','m','o'],
+    ];
+    
+    console.log(arrays);
+    console.log(arrays[0][0],arrays[1][1],arrays[2][2],arrays[3][3],arrays[4][4]);
+    console.log(arrays[0][4],arrays[1][3],arrays[2][2],arrays[3][1],arrays[4][0]);
+    
